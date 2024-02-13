65500a4524a85aa7ec394eae9d3ff989908a2627
Michael DeWitt <me.dewitt.jr@gmail.com>
2024-02-13 08:12:33 -0500

    update for new metrics

--- read.py
+++ read.py
@@ -13,12 +13,20 @@ dir_list = os.listdir('data')
 
 for file in dir_list:
     df = read_table(os.path.join('data', file))
+    print(df.columns)
     
     if df.columns.isin(['wwtp_name']).any():
         file_time = datetime.datetime.now().strftime("%Y-%m-%d_")
         file_name = file_time + r'raw.csv' 
         df.to_csv(os.path.join("output",file_name), index=False)
         df.to_csv(os.path.join("output",'latest.csv'), index=False)
+        print("Got it!")
+        
+    if df.columns.isin(['RSV-like']).any():
+        file_time = datetime.datetime.now().strftime("%Y-%m-%d_")
+        file_name = file_time + r'raw.csv' 
+        df.to_csv(os.path.join("ili",file_name), index=False)
+        df.to_csv(os.path.join("ili",'latest.csv'), index=False)
         print("Got it!")
     else:
         print("Not it.")
